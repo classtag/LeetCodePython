0686993e2d9db5312941e18796bb0e8a956f071d
classtag <anduo@qq.com>
2018-11-03 13:54:38 +0800

    add more hash map examples

---
+++ powx-n.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Solution:
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        # 2^10
---
+++ three-sum.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# 15 https://leetcode.com/problems/3sum/description/
+
+
+class Solution:
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
